49f018df303c267833cc4a29e3a4c5be2c211b6a
colewinfield <Coltonwinfield@gmail.com>
2021-09-24 14:17:44 -0700

    Refactored and added NavGraph Jetpack component.

--- app/src/main/java/edu/fsu/equidistant/fragments/LoginFragment.kt
+++ app/src/main/java/edu/fsu/equidistant/fragments/LoginFragment.kt
@@ -1,79 +1,44 @@
 package edu.fsu.equidistant.fragments
 
-import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import android.widget.Button
-import android.widget.EditText
+import android.widget.Toast
+import androidx.navigation.fragment.findNavController
 import edu.fsu.equidistant.R
+import edu.fsu.equidistant.databinding.FragmentLoginBinding
 
 
-class LoginFragment : Fragment() {
+class LoginFragment : Fragment(R.layout.fragment_login) {
 
-    private var listener : LoginFragment.LoginInterface? = null
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+        val binding = FragmentLoginBinding.bind(view)
+        binding.apply {
+            submitButton.setOnClickListener {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+                val username = etUsername.text.toString()
+                val password = etPassword.text.toString()
 
-    }
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        val rootView : View = inflater.inflate(R.layout.fragment_login, container, false)
-        val submit: Button = rootView.findViewById(R.id.submit)
-        val user: EditText = rootView.findViewById(R.id.et_username)
-        val pw: EditText = rootView.findViewById(R.id.et_password)
-        submit.setOnClickListener{
-            if (validate(user,pw)) {
-                //TODO implement backend on FireBase
-                listener?.onSubmit(user.text.toString(), pw.text.toString())
+                // TODO: Make a proper validation with Firebase integration
+                if (isUsernameOrPasswordEmpty(username, password)) {
+                    val toast = Toast
+                        .makeText(context, "Username or Password cannot be empty", Toast.LENGTH_LONG)
+                    toast.show()
+                } else {
+                    val action = LoginFragmentDirections.actionLoginFragmentToHomeFragment()
+                    findNavController().navigate(action)
+                }
             }
-
-
         }
 
-        return rootView
     }
 
-    //TODO Make more in depth input validation
-    private fun validate(username:EditText,password:EditText):Boolean{
-        var flag = true
-        if (username.text.toString().isEmpty() || username.text.toString().trim().isEmpty()){
-            username.error = "Username cannot be empty"
-            flag = false
-        }
-        if (password.text.toString().trim().isEmpty()){
-            password.error = "Password cannot be empty"
-            flag = false
-        }
-        return flag
-    }
 
-    override fun onAttach(context: Context) {
-        super.onAttach(context)
-        if (context is LoginFragment.LoginInterface){
-            listener = context
-        }
-        else{
-            throw ClassCastException(
-                context.toString() + "must implement interface"
-            )
-        }
-    }
+    private fun isUsernameOrPasswordEmpty(username: String, password: String) =
+        username.isEmpty() || password.isEmpty()
 
-    override fun onDetach() {
-        super.onDetach()
-        listener = null
-    }
 
-    interface LoginInterface{
-        fun onSubmit(username:String,password:String)
-    }
+
 }
--- app/src/main/java/edu/fsu/equidistant/fragments/RegisterFragment.kt
+++ app/src/main/java/edu/fsu/equidistant/fragments/RegisterFragment.kt
@@ -1,94 +1,54 @@
 package edu.fsu.equidistant.fragments
 
-import android.content.Context
 import android.os.Bundle
 import android.telephony.PhoneNumberUtils
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import android.widget.Button
-import android.widget.EditText
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import edu.fsu.equidistant.R
+import edu.fsu.equidistant.databinding.FragmentRegisterBinding
 
-class RegisterFragment : Fragment() {
+class RegisterFragment : Fragment(R.layout.fragment_register) {
 
-    var listener : RegisterInterface? = null
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+        val binding = FragmentRegisterBinding.bind(view)
+        binding.apply {
+            submitButton.setOnClickListener {
+                val username = etUsername.text.toString()
+                val password = etPassword.text.toString()
+                val confirmPassword = etValidate.text.toString()
+                val number = etPhoneNumber.text.toString()
 
-
-    }
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        val rootView: View = inflater.inflate(R.layout.fragment_register, container, false)
-
-        val username: EditText = rootView.findViewById(R.id.et_username)
-        val password: EditText = rootView.findViewById(R.id.et_password)
-        val pw_validate: EditText = rootView.findViewById(R.id.et_validate)
-        val phone :EditText = rootView.findViewById(R.id.et_phoneNumber)
-        val submit: Button =rootView.findViewById(R.id.submit)
-
-        //TODO implement backend on FireBase
-        submit.setOnClickListener{
-            if (validate(username,password,pw_validate,phone)) {
-                listener?.onSubmit(
-                    username.text.toString(),
-                    password.text.toString(),
-                    phone.text.toString()
-                )
+                if (validate(username, password, confirmPassword, number)) {
+                    val action = RegisterFragmentDirections.actionRegisterFragmentToHomeFragment()
+                    findNavController().navigate(action)
+                }
             }
         }
-
-        return rootView
     }
 
-    override fun onAttach(context: Context) {
-        super.onAttach(context)
-        if (context is RegisterFragment.RegisterInterface){
-            listener = context
-        }
-        else{
-            throw ClassCastException(
-                context.toString() + "must implement interface"
-            )
-        }
-    }
+    // TODO: Implement Firebase validation
+    private fun validate(
+        username: String, password: String,
+        confirmPassword: String, number: String
+    ): Boolean {
 
-    //TODO implement more robust error checking
-    fun validate(username: EditText,password: EditText,valid_pw: EditText,cell: EditText):Boolean{
-        var valid = true
-        if(username.text.toString().trim().isEmpty()){
-            username.error = "Username field required"
-            valid = false
+        if (username.isEmpty() || password.isEmpty()
+            || confirmPassword.isEmpty() || number.isEmpty()) {
+            Toast.makeText(context, "Fields cannot be empty", Toast.LENGTH_LONG).show()
+            return false
+        } else if (password != confirmPassword) {
+            Toast.makeText(context, "Passwords don't match", Toast.LENGTH_LONG).show()
+            return false
+        } else if (!PhoneNumberUtils.isGlobalPhoneNumber(number)) {
+            Toast.makeText(context, "Invalid number", Toast.LENGTH_LONG).show()
+            return false
         }
-        if (password.text.toString().trim().isEmpty()){
-            password.error = "Password field required"
-            valid = false
-        }
-        if (! valid_pw.text.toString().equals(password.text.toString())){
-            valid_pw.error = "Passwords do not match"
-            valid = false
-        }
-        if (! PhoneNumberUtils.isGlobalPhoneNumber(cell.text.toString())){
-            cell.error = "Invalid phone number"
-            valid = false
-        }
-        return valid
-    }
 
-    override fun onDetach() {
-        super.onDetach()
-        listener = null
-    }
-
-    interface RegisterInterface{
-        fun onSubmit(username:String,password:String,phoneNumber:String)
+        return true
     }
 
 }
