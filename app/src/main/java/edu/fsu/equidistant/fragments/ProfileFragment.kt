4d785e875a66ae26dba3d7ed72489396c5dc1775
Ryan <rf19l@my.fsu.edu>
2021-10-10 15:20:22 -0400

    Added firebase storage api to upload images to firebase storage.  Need to connect cloud firestore to storage

--- app/src/main/java/edu/fsu/equidistant/fragments/ProfileFragment.kt
+++ app/src/main/java/edu/fsu/equidistant/fragments/ProfileFragment.kt
@@ -1,12 +1,12 @@
 package edu.fsu.equidistant.fragments
 
-import android.app.Activity
-import android.content.Intent
-import android.content.Intent.ACTION_PICK
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.net.Uri
 import android.os.Bundle
 import android.view.*
+import android.widget.Toast
 import androidx.activity.result.ActivityResultCallback
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.result.contract.ActivityResultContracts.GetContent
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
@@ -15,17 +15,22 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.storage.ktx.storage
-import edu.fsu.equidistant.MainActivity
 import edu.fsu.equidistant.R
 import edu.fsu.equidistant.databinding.FragmentProfileBinding
+import java.io.ByteArrayOutputStream
 
 class ProfileFragment : Fragment(R.layout.fragment_home) {
 
 
     private val storage = Firebase.storage
+    val storageRef = storage.reference
     private val args: ProfileFragmentArgs by navArgs()
     private var binding: FragmentProfileBinding? = null
     private var user: FirebaseUser? = null
+    private var imageUri: Uri? = null
+
+
+
 
     override fun onCreateView(
 
@@ -40,7 +45,7 @@ class ProfileFragment : Fragment(R.layout.fragment_home) {
         return view
     }
 
-
+//TODO Clean up filepath and integrate with firestore to link account to photo
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setHasOptionsMenu(true)
@@ -49,6 +54,23 @@ class ProfileFragment : Fragment(R.layout.fragment_home) {
             GetContent(),
             ActivityResultCallback {
                 binding?.imageViewProfilePhoto?.setImageURI(it)
+                imageUri = it
+                val imageRef = storageRef.child("users/" + user + "/" + imageUri)
+                val baos = ByteArrayOutputStream()
+                binding?.let { it1 -> getBitmapFromView(it1.imageViewProfilePhoto) }
+                    ?.compress(Bitmap.CompressFormat.JPEG, 100, baos)
+                val data = baos.toByteArray()
+
+                val uploadTask = imageRef.putBytes(data)
+                uploadTask.addOnFailureListener {
+                    // Handle unsuccessful upload
+                    Toast.makeText(context,it.message,Toast.LENGTH_LONG).show()
+                }.addOnSuccessListener { taskSnapshot ->
+                    // taskSnapshot.metadata contains file metadata such as size, content-type, etc.
+                    // ...
+                    Toast.makeText(context,"Upload Success",Toast.LENGTH_LONG).show()
+
+                }
             })
         binding!!.apply{
             imageViewProfilePhoto.setOnClickListener {
@@ -94,6 +116,16 @@ class ProfileFragment : Fragment(R.layout.fragment_home) {
         binding = null
         user = null
     }
+
+    private fun getBitmapFromView(view: View): Bitmap? {
+        val bitmap =
+            Bitmap.createBitmap(view.width, view.height, Bitmap.Config.ARGB_8888)
+        val canvas = Canvas(bitmap)
+        view.draw(canvas)
+        return bitmap
+    }
+
+
 
 
 }
