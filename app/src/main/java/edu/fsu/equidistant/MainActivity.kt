70a3e62e1152fd83d435aefcca5aa6ce3ebd273c
colewinfield <Coltonwinfield@gmail.com>
2021-11-05 22:35:44 -0700

    Refactored a lot of the fragments. Got the token for each user. Working on push notifications.

--- app/src/main/java/edu/fsu/equidistant/MainActivity.kt
+++ app/src/main/java/edu/fsu/equidistant/MainActivity.kt
@@ -1,82 +1,15 @@
 package edu.fsu.equidistant
 
-import android.content.ComponentName
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuInflater
-import android.view.MenuItem
-import com.google.firebase.auth.FirebaseAuth
-import kotlin.system.exitProcess
-import android.Manifest
-import android.content.BroadcastReceiver
-import android.content.Context
-import android.content.IntentFilter
-import android.content.ServiceConnection
-import android.content.SharedPreferences
-import android.content.pm.PackageManager
-import android.location.Location
-import android.net.Uri
-import android.os.IBinder
-import android.provider.Settings
-import android.util.AttributeSet
-import android.util.Log
-import android.view.View
-import android.widget.Button
-import android.widget.TextView
-import android.widget.Toast
-import androidx.core.app.ActivityCompat
-import androidx.localbroadcastmanager.content.LocalBroadcastManager
-import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
 
-//private const val TAG = "MainActivity"
-private const val REQUEST_FOREGROUND_ONLY_PERMISSIONS_REQUEST_CODE = 34
-
-class MainActivity : AppCompatActivity(){
-
-    companion object{
-        val TAG: String = MainActivity::class.java.simpleName
-        var PERMISSIONS = arrayOf(
-            Manifest.permission.ACCESS_FINE_LOCATION,
-            Manifest.permission.ACCESS_COARSE_LOCATION,
-            REQUEST_FOREGROUND_ONLY_PERMISSIONS_REQUEST_CODE
-        )
-    }
-
-
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-    }
-    /*
-        Menu Functions
-     */
-    override fun onPrepareOptionsMenu(menu: Menu):Boolean{
-        val logout: MenuItem = menu.findItem(R.id.option_logout)
-        val editProfile: MenuItem = menu.findItem(R.id.option_editProfile);
-        logout.setVisible(FirebaseAuth.getInstance().getCurrentUser() != null);
-        editProfile.setVisible(FirebaseAuth.getInstance().getCurrentUser() !=null);
-        return true;
     }
 
-
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        val inflater: MenuInflater = menuInflater
-        inflater.inflate(R.menu.options_menu, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        val id = item.itemId
-        if (id == R.id.option_quit){
-            finishAndRemoveTask()
-            exitProcess(0)
-        }
-        return super.onOptionsItemSelected(item)
-    }
 
 
 }
--- app/src/main/java/edu/fsu/equidistant/fragments/HomeFragment.kt
+++ app/src/main/java/edu/fsu/equidistant/fragments/HomeFragment.kt
@@ -1,111 +1,26 @@
 package edu.fsu.equidistant.fragments
 
-import android.Manifest
-import android.content.*
-import android.content.pm.PackageManager
-import android.location.Location
-import android.net.Uri
 import android.os.Bundle
-import android.os.IBinder
-import android.provider.Settings
-import android.util.Log
-import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
 import android.view.MenuItem
 import android.view.View
-import android.view.ViewGroup
-import android.widget.Toast
-import androidx.activity.result.contract.ActivityResultContracts
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
-import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
-import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.firestore.GeoPoint
-import edu.fsu.equidistant.*
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.storage.FirebaseStorage
+import edu.fsu.equidistant.R
 import edu.fsu.equidistant.databinding.FragmentHomeBinding
-
-private const val TAG = "HomeFragment"
+import kotlin.system.exitProcess
 
 class HomeFragment : Fragment(R.layout.fragment_home) {
 
-    private var locationServiceBound = false
-
-
-    val requestPermissionLauncher =
-        registerForActivityResult(
-            ActivityResultContracts.RequestPermission()
-        ) { isGranted: Boolean ->
-            if (isGranted) {
-                Log.d("DEBUG","permission granted")
-                locationService?.subscribeToLocationUpdates()
-
-            } else {
-                Log.d("DEBUG","permission denied")
-                Snackbar.make(
-                    requireView().findViewById(R.id.fragment_home),
-                    R.string.permission_denied_explanation,
-                    Snackbar.LENGTH_LONG
-                )
-                    .setAction(R.string.settings) {
-                        // Build intent that displays the App settings screen.
-                        val intent = Intent()
-                        intent.action = Settings.ACTION_APPLICATION_DETAILS_SETTINGS
-                        val uri = Uri.fromParts(
-                            "package",
-                            BuildConfig.APPLICATION_ID,
-                            null
-                        )
-                        intent.data = uri
-                        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
-                        startActivity(intent)
-                    }
-                    .show()
-
-            }
-        }
-
-
     private val args: HomeFragmentArgs by navArgs()
-
+    private val database: FirebaseFirestore = FirebaseFirestore.getInstance()
     private var binding: FragmentHomeBinding? = null
 
-    // Provides location updates for while-in-use feature.
-    private var locationService: LocationService? = null
-
-    // Monitors connection to the while-in-use service.
-    private val foregroundOnlyServiceConnection = object : ServiceConnection {
-
-        override fun onServiceConnected(name: ComponentName, service: IBinder) {
-            val binder = service as LocationService.LocalBinder
-            locationService = binder.service
-            if (foregroundPermissionApproved()){
-            locationService!!.subscribeToLocationUpdates()}
-           // locationService!!.unsubscribeToLocationUpdates()
-           // locationService!!.subscribeToLocationUpdates()
-        }
-
-        override fun onServiceDisconnected(name: ComponentName) {
-            locationService = null
-        }
-    }
-
-    // Keeps track of users location sharing preferences
-    private lateinit var sharedPreferences: SharedPreferences
-
-    // Listens for location broadcasts from LocationService.
-    private lateinit var locationServiceBroadcastReceiver: HomeFragment.LocationServiceBroadcastReceiver
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        locationServiceBroadcastReceiver = LocationServiceBroadcastReceiver()
-        requestPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
-
-    }
-
-    //TODO CLEAN UP, DONT NEED ANYMORE
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setHasOptionsMenu(true)
@@ -114,135 +29,38 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
             textViewEmail.text = args.email
             textViewUserid.text = args.userId
         }
-        sharedPreferences=
-            activity?.getSharedPreferences(getString(R.string.preference_file_key),Context.MODE_PRIVATE)!!
-        when {
-            ContextCompat.checkSelfPermission(
-                requireContext(),
-                Manifest.permission.ACCESS_FINE_LOCATION
-            ) == PackageManager.PERMISSION_GRANTED -> {
-                // You can use the API that requires the permission.
-            }
-            shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION) -> {
-                Snackbar.make(
-                    requireView().findViewById(R.id.root_container),
-                    R.string.permission_denied_explanation,
-                    Snackbar.LENGTH_LONG
-                )
-                    .setAction(R.string.settings) {
-                        // Build intent that displays the App settings screen.
-                        val intent = Intent()
-                        intent.action = Settings.ACTION_APPLICATION_DETAILS_SETTINGS
-                        val uri = Uri.fromParts(
-                            "package",
-                            BuildConfig.APPLICATION_ID,
-                            null
-                        )
-                        intent.data = uri
-                        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
-                        startActivity(intent)
-                    }
-                    .show()
-            }
-        }
-
     }
-
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        val id = item.itemId
-        if (id == R.id.option_logout) {
-            FirebaseAuth.getInstance().signOut()
-
-            val action = HomeFragmentDirections.actionHomeFragmentToLoginFragment()
-            findNavController().navigate(action)
-
-
-        } else if (id == R.id.option_editProfile) {
-            val action = HomeFragmentDirections.actionHomeFragmentToProfileFragment(args.userId)
-            findNavController().navigate(action)
-        }
-        return super.onOptionsItemSelected(item)
-
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        binding = null
-    }
-
-
-    // TODO
-    //  Do I need this to start the service? I am not setting any views or doing anyting with the data in the fragment?
-    /**
-     * Receiver for location broadcasts from [ForegroundOnlyLocationService].
-     */
-    private inner class LocationServiceBroadcastReceiver : BroadcastReceiver() {
-
-        override fun onReceive(context: Context, intent: Intent) {
-            val location = intent.getParcelableExtra<Location>(
-                LocationService.EXTRA_LOCATION
-            )
-            if (location != null) {
-                logResultsToScreen(location.toText())
+        return when (item.itemId) {
+            R.id.option_logout -> {
+                clearToken(FirebaseAuth.getInstance().currentUser!!.uid)
+                FirebaseAuth.getInstance().signOut()
+                val action = HomeFragmentDirections.actionHomeFragmentToLoginFragment()
+                findNavController().navigate(action)
+                true
             }
-
+            R.id.option_editProfile -> {
+                val action = HomeFragmentDirections.actionHomeFragmentToProfileFragment(args.userId)
+                findNavController().navigate(action)
+                true
+            }
+            R.id.option_quit -> {
+                exitProcess(0)
+            }
+            else -> super.onOptionsItemSelected(item)
         }
-        // for debug
-        private fun logResultsToScreen(output: String) {
-            Log.d(TAG,output)
-            Toast.makeText(context,output, Toast.LENGTH_LONG).show()
-        }
     }
 
-    private fun onLocationChanged(location: Location): GeoPoint {
-        val lat = (location.latitude * 1E6).toInt()
-        val lng = (location.longitude * 1E6).toInt()
-        val point = GeoPoint(lat.toDouble(), lng.toDouble())
-        return point
+    // create that menu
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.options_menu, menu)
     }
 
-    private fun foregroundPermissionApproved(): Boolean {
-        return PackageManager.PERMISSION_GRANTED == ActivityCompat.checkSelfPermission(
-            requireContext(),
-            Manifest.permission.ACCESS_FINE_LOCATION
-        )
+    private fun clearToken(userId: String) {
+        database.collection("users")
+            .document(userId)
+            .update("token", "")
     }
-
-
-    override fun onStart() {
-        super.onStart()
-        val serviceIntent = Intent(context, LocationService::class.java)
-        requireActivity().bindService(serviceIntent, foregroundOnlyServiceConnection, Context.BIND_AUTO_CREATE)
-    }
-
-    override fun onResume() {
-        super.onResume()
-        LocalBroadcastManager.getInstance(requireContext()).registerReceiver(
-            locationServiceBroadcastReceiver,
-            IntentFilter(
-                LocationService.ACTION_LOCATION_SERVICE_BROADCAST)
-        )
-    }
-
-    override fun onPause() {
-        LocalBroadcastManager.getInstance(requireContext()).unregisterReceiver(
-            locationServiceBroadcastReceiver
-        )
-        super.onPause()
-    }
-
-    override fun onStop() {
-        if (locationServiceBound) {
-            requireActivity().unbindService(foregroundOnlyServiceConnection)
-            locationServiceBound = false
-        }
-        super.onStop()
-    }
-
-
-
-
-
 }
 
