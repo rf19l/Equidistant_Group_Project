bd55d5a97002688e3fefebd9fcce9a49251fd26f
colewinfield <Coltonwinfield@gmail.com>
2021-11-13 20:53:23 -0800

    Adding BottomNavigation. . .

--- app/src/main/java/edu/fsu/equidistant/MainActivity.kt
+++ app/src/main/java/edu/fsu/equidistant/MainActivity.kt
@@ -1,15 +1,58 @@
 package edu.fsu.equidistant
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlin.system.exitProcess
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment)
+                as NavHostFragment
+
+        val navController = navHostFragment.navController
+        val bottomNavigationView: BottomNavigationView = findViewById(R.id.bottom_navigation)
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            if (destination.id == R.id.loginFragment || destination.id == R.id.registerFragment) {
+                bottomNavigationView.visibility = View.GONE
+            } else {
+                bottomNavigationView.visibility = View.VISIBLE
+            }
+        }
     }
 
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.option_home -> {
 
+                true
+            }
+            R.id.option_meeting -> {
+
+                true
+            }
+            R.id.option_navigation -> {
+                exitProcess(0)
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.bottom_menu_bar, menu)
+        return true
+    }
 
 }
